f3cb580f119e3af557c0faeb2204bd98348d2e0e
chanminmaxscaler <jcm04@naver.com>
2022-11-25 17:47:05 +0900

    feat: setup precourse onboarding exercise

---
+++ src/main/java/precourse/week1/problem1/Problem1.java
@@ -0,0 +1,14 @@
+package precourse.week1.problem1;
+
+import java.util.List;
+
+public class Problem1 {
+    public static int solution(List<Integer> pobi, List<Integer> crong) {
+        int answer = Integer.MAX_VALUE;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
