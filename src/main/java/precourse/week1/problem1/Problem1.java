0419423c93600ff76d8d49d3995717b6d612ef5b
chanminmaxscaler <jcm04@naver.com>
2022-11-30 21:03:35 +0900

    feat: Problem1 클래스의 solution 메소드가 테스트 코드를 통과하도록 구현한다

--- src/main/java/precourse/week1/problem1/Problem1.java
+++ src/main/java/precourse/week1/problem1/Problem1.java
@@ -1,14 +1,12 @@
 package precourse.week1.problem1;
 
+import precourse.week1.problem1.controller.BookGameController;
+
 import java.util.List;
 
 public class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
-    }
-
-    public static void main(String[] args) {
-        System.out.println("hello world!");
+        BookGameController bookGameController = new BookGameController();
+        return bookGameController.runGameMain(pobi, crong);
     }
 }
