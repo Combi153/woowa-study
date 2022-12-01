a6e02367b33400304f23161bdbea202c607f0864
chanminmaxscaler <jcm04@naver.com>
2022-12-01 17:51:21 +0900

    feat: Problem3 클래스에서 controller에 접근해 게임을 실행하도록 구현한다

--- src/main/java/precourse/week1/problem3/Problem3.java
+++ src/main/java/precourse/week1/problem3/Problem3.java
@@ -1,8 +1,11 @@
 package precourse.week1.problem3;
 
+import precourse.week1.problem3.controller.GameOf369Controller;
+
 public class Problem3 {
 
     public static int solution(int number) {
-        return -1;
+        GameOf369Controller controller = new GameOf369Controller();
+        return controller.run369GameUntil(number);
     }
 }
