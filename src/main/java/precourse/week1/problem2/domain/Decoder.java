35c5a06536a745c5b736ba0a847854ff9137d69c
chanminmaxscaler <jcm04@naver.com>
2022-12-01 14:27:59 +0900

    refactor: Decoder 클래스의 validate 메소드를 리팩토링한다
    매직넘버를 없애고 상수를 사용한다.
    추상화 수준을 맞추도록 isOutOfRange, isContainingOnlyLowercase 메소드를 사용한다.

--- src/main/java/precourse/week1/problem2/domain/Decoder.java
+++ src/main/java/precourse/week1/problem2/domain/Decoder.java
@@ -2,6 +2,8 @@ package precourse.week1.problem2.domain;
 
 public class Decoder {
 
+    private static final int START_OF_RANGE = 1;
+    private static final int END_OF_RANGE = 1000;
     private final String cryptogram;
 
     public Decoder(String cryptogram) {
@@ -9,19 +11,26 @@ public class Decoder {
         this.cryptogram = cryptogram;
     }
 
-    void validate(String cryptogram) {
-        int length = cryptogram.length();
-        if (length < 1 || length > 1000) {
+    private void validate(String cryptogram) {
+        if (isOutOfRange(cryptogram.length())) {
             throw new IllegalArgumentException("[ERROR] 암호의 길이는 1 이상 1000 이하여야 합니다.");
         }
-        if (!cryptogram.matches("[a-z]{1,}")) {
+        if (!isContainingOnlyLowercase(cryptogram)) {
             throw new IllegalArgumentException("[ERROR] 암호는 소문자만 가능합니다.");
         }
     }
 
+    private boolean isOutOfRange(int length) {
+        return length < START_OF_RANGE || length > END_OF_RANGE;
+    }
+
+    private boolean isContainingOnlyLowercase(String cryptogram) {
+        return cryptogram.matches("[a-z]+");
+    }
+
     public String decodeCryptogram() {
         String decodedCryptogram = cryptogram;
-        while(hasConsecutiveDuplicate(decodedCryptogram)) {
+        while (hasConsecutiveDuplicate(decodedCryptogram)) {
             decodedCryptogram = removeConsecutiveDuplicate(decodedCryptogram);
         }
         return decodedCryptogram;
