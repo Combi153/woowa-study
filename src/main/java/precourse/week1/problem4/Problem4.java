a5af7309afd43d2ebefc30e6c46632ff6edacfd2
chanminmaxscaler <jcm04@naver.com>
2022-12-04 16:56:47 +0900

    feat: Problem4 클래스의 solution 메소드가 동작하도록 내부를 작성한다

--- src/main/java/precourse/week1/problem4/Problem4.java
+++ src/main/java/precourse/week1/problem4/Problem4.java
@@ -1,7 +1,11 @@
 package precourse.week1.problem4;
 
+import precourse.week1.problem4.domain.ReverseDictionary;
+import precourse.week1.problem4.domain.Word;
+
 public class Problem4 {
     public static String solution(String word) {
-        return "";
+        ReverseDictionary dictionary = new ReverseDictionary();
+        return dictionary.translate(new Word(word));
     }
 }
