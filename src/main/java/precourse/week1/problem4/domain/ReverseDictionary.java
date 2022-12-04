c2634e496f756257144c507402b37b894b75ecea
chanminmaxscaler <jcm04@naver.com>
2022-12-05 00:15:54 +0900

    fix: 조건문의 내용을 수정한다

--- src/main/java/precourse/week1/problem4/domain/ReverseDictionary.java
+++ src/main/java/precourse/week1/problem4/domain/ReverseDictionary.java
@@ -2,8 +2,8 @@ package precourse.week1.problem4.domain;
 
 public class ReverseDictionary {
 
-    private static final Range UPPER_CASE = (number) -> number >= AsciiCode.START_OF_UPPER_CASE.getNumber() || number <= AsciiCode.END_OF_UPPER_CASE.getNumber();
-    private static final Range LOWER_CASE = (number) -> number >= AsciiCode.START_OF_LOWER_CASE.getNumber() || number <= AsciiCode.END_OF_LOWER_CASE.getNumber();
+    private static final Range UPPER_CASE = (number) -> number >= AsciiCode.START_OF_UPPER_CASE.getNumber() && number <= AsciiCode.END_OF_UPPER_CASE.getNumber();
+    private static final Range LOWER_CASE = (number) -> number >= AsciiCode.START_OF_LOWER_CASE.getNumber() && number <= AsciiCode.END_OF_LOWER_CASE.getNumber();
     private static final Translator UPPER_CASE_TRANSLATOR = (original) -> AsciiCode.START_OF_UPPER_CASE.getNumber() + AsciiCode.END_OF_UPPER_CASE.getNumber() - original;
     private static final Translator LOWER_CASE_TRANSLATOR = (original) -> AsciiCode.START_OF_LOWER_CASE.getNumber() + AsciiCode.END_OF_LOWER_CASE.getNumber() - original;
 
