0a4a9e6e349a5337cdd5869bde0105e3e4ff3791
chanminmaxscaler <jcm04@naver.com>
2022-12-05 00:16:40 +0900

    refactor: 테스트 코드를 통과하도록 코드를 수정한다

--- src/main/java/precourse/week1/problem4/Problem4.java
+++ src/main/java/precourse/week1/problem4/Problem4.java
@@ -6,6 +6,8 @@ import precourse.week1.problem4.domain.Word;
 public class Problem4 {
     public static String solution(String word) {
         ReverseDictionary dictionary = new ReverseDictionary();
-        return dictionary.translate(new Word(word));
+        Word original = new Word(word);
+        Word reversed = dictionary.reverse(original);
+        return reversed.getWord();
     }
 }
