9fd54b44a4908e5499a0986340203c87cdea8d74
chanminmaxscaler <jcm04@naver.com>
2022-12-04 23:57:58 +0900

    feat: Range, Translator 인터페이스로 함수형 인터페이스를 구현한다

---
+++ src/main/java/precourse/week1/problem4/domain/Range.java
@@ -0,0 +1,5 @@
+package precourse.week1.problem4.domain;
+
+public interface Range {
+    boolean contains(int number);
+}
---
+++ src/main/java/precourse/week1/problem4/domain/Translator.java
@@ -0,0 +1,6 @@
+package precourse.week1.problem4.domain;
+
+@FunctionalInterface
+public interface Translator {
+    int translate(int original);
+}
