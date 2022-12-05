5c2b66f5631ef785a63e7ba15718bae3b1aa5a6c
chanminmaxscaler <jcm04@naver.com>
2022-12-05 14:49:59 +0900

    feat: OutputView 클래스에 문구 출력 기능을 구현한다
    게임 시작 문구, 판정 결과 문구, 게임 결과 문구를 출력하는 메소드들을 각각 구현한다.

---
+++ src/main/java/precourse/week2/view/OutputView.java
@@ -0,0 +1,28 @@
+package precourse.week2.view;
+
+import precourse.week2.domain.Decision;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OutputView {
+
+    public void printStartMessage() {
+        System.out.print("숫자 야구 게임을 시작합니다.\n");
+    }
+
+    public void printDecisionMessage(Decision decision) {
+        List<String> messages = new ArrayList<>();
+        if (decision.hasBall()) {
+            messages.add(String.format("%s볼", decision.getBall()));
+        }
+        if (decision.hasStrike()) {
+            messages.add(String.format("%s스트라이크", decision.getStrike()));
+        }
+        System.out.print(String.join(" ", messages) + "\n");
+    }
+
+    public void printAnswerMessage() {
+        System.out.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+}
