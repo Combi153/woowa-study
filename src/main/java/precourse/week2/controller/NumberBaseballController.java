e2e987ad9d690d99d232831bc3f97842ae1d34dc
chanminmaxscaler <jcm04@naver.com>
2022-12-05 17:28:03 +0900

    feat: NumberBaseballController 클래스에 숫자 재입력 기능, 게임 재시작 기능을 구현한다
    runGameSystem 메소드를 통해 게임 재시작 기능을 구현한다.
    Command 객체가 재시도인 경우 게임을 재시작하면서, 랜덤 숫자를 다시 생성할 수 있도록 한다.
    Command 객체가 재시도가 아닐 경우 반복문을 빠져나갈 수 있도록 한다.
    
    runNumberBaseballGame 메소드를 통해 숫자 재입력 기능을 구현한다.
    사용자의 숫자와 컴퓨터의 숫자를 비교한 판정이 Full Strike가 아닌 경우 사용자의 입력을 다시 받도록 한다.

---
+++ src/main/java/precourse/week2/controller/NumberBaseballController.java
@@ -0,0 +1,60 @@
+package precourse.week2.controller;
+
+import precourse.week2.domain.*;
+import precourse.week2.view.InputView;
+import precourse.week2.view.OutputView;
+
+public class NumberBaseballController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public NumberBaseballController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void runMainSystem() {
+        try {
+            runGameSystem();
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e.getMessage());
+            throw e;
+        }
+    }
+
+    private void runGameSystem() {
+        outputView.printStartMessage();
+        Command command = new Command();
+        Computer computer = new Computer();
+
+        while (command.isStart()) {
+            NumberBall computerBall = computer.createNumberBall();
+            runNumberBaseballGame(computerBall);
+            outputView.printAnswerMessage();
+            command = receiveCommand();
+        }
+    }
+
+    private void runNumberBaseballGame(NumberBall computerBall) {
+        Decision decision;
+        do {
+            NumberBall userBall = receiveUserBall();
+            decision = receiveDecision(computerBall, userBall);
+            outputView.printDecisionMessage(decision);
+        } while (!decision.isFullStrike());
+    }
+
+    private Decision receiveDecision(NumberBall computerBall, NumberBall userBall) {
+        Referee referee = new Referee();
+        return referee.createDecision(userBall, computerBall);
+    }
+
+    private NumberBall receiveUserBall() {
+        return new NumberBall(inputView.readNumberBall());
+    }
+
+    private Command receiveCommand() {
+        return new Command(inputView.readCommand());
+    }
+}
