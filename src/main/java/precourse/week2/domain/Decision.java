22974777c1651d0099b2bdc61bc8b2732ba4b312
chanminmaxscaler <jcm04@naver.com>
2022-12-05 17:35:14 +0900

    feat: Decision 클래스에 메소드를 추가한다
    isFullStrike 메소드는 Decision 객체가 Full Strike 판정인지 확인한다.
    hasBall, hasStrike 메소드는 각각 Decision 객체가 Ball, Strike 판정을 갖고 있는지 확인한다.
    hasNothing 메소드는 Decision 객체가 Ball, Strike 판정을 갖고 있는지 확인한다.
    getStrike, getBall 메소드는 판정 출력을 위해 사용하는 getter 메소드이다.

--- src/main/java/precourse/week2/domain/Decision.java
+++ src/main/java/precourse/week2/domain/Decision.java
@@ -2,11 +2,37 @@ package precourse.week2.domain;
 
 public class Decision {
 
+    private static final int NOTHING = 0;
+    private static final int FULL_STRIKE = 3;
     private final int strike;
     private final int ball;
 
     public Decision(int strike, int ball) {
         this.strike = strike;
         this.ball = ball;
+    }
+
+    public boolean isFullStrike() {
+        return strike == FULL_STRIKE;
+    }
+
+    public boolean hasBall() {
+        return ball != NOTHING;
+    }
+
+    public boolean hasStrike() {
+        return strike != NOTHING;
+    }
+
+    public boolean hasNothing() {
+        return ball == NOTHING && strike == NOTHING;
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public int getBall() {
+        return ball;
     }
 }
