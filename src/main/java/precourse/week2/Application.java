5c6ade948d3cc7383f989cc49feba7fe157126ce
chanminmaxscaler <jcm04@naver.com>
2022-12-05 17:29:32 +0900

    feat: Application 클래스에서 숫자야구게임을 시작하는 기능을 구현한다
    NumberBaseballController 객체의 runMainSystem 메소드를 실행하여 전체 게임시스템을 시작할 수 있도록 한다.

--- src/main/java/precourse/week2/Application.java
+++ src/main/java/precourse/week2/Application.java
@@ -1,7 +1,12 @@
 package precourse.week2;
 
+import precourse.week2.controller.NumberBaseballController;
+import precourse.week2.view.InputView;
+import precourse.week2.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        NumberBaseballController controller = new NumberBaseballController(new InputView(), new OutputView());
+        controller.runMainSystem();
     }
 }
