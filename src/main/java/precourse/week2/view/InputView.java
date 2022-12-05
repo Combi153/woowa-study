2b7c0de478e50d7168b02d7cf4b9a5e832ba34cd
chanminmaxscaler <jcm04@naver.com>
2022-12-05 14:39:39 +0900

    feat: InputView 클래스에 게임 커맨드 입력 기능을 구현한다
    inputCommand 메소드를 통해 커맨드 입력 안내 문구를 출력한 후, Console.readLine 메소드를 활용해 사용자의 입력을 받아 반환하도록 한다.

--- src/main/java/precourse/week2/view/InputView.java
+++ src/main/java/precourse/week2/view/InputView.java
@@ -12,4 +12,14 @@ public class InputView {
     private void printNumberBallGuide() {
         System.out.print("숫자를 입력해주세요 : ");
     }
+
+
+    public String inputCommand() {
+        printCommandGuide();
+        return Console.readLine();
+    }
+
+    private void printCommandGuide() {
+        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+    }
 }
