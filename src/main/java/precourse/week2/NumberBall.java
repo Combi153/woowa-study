f3fd9c3899dfd75e24d5319c3c6de55b4420e04f
chanminmaxscaler <jcm04@naver.com>
2022-12-05 11:15:06 +0900

    feat: NumberBall 클래스에 숫자 야구공을 검증하는 기능을 구현한다
    validate 메소드를 통해 숫자 야구공이 세 개의 숫자로 구성되었는지, 서로 중복되었는지 검증한다.
    만일 세 개의 숫자가 아니거나, 서로 중복이 되는 경우 IllegalArgumentException 을 발생시킨다.

---
+++ src/main/java/precourse/week2/NumberBall.java
@@ -0,0 +1,30 @@
+package precourse.week2;
+
+public class NumberBall {
+
+    private final String number;
+
+    public NumberBall(String number) {
+        validate(number);
+        this.number = number;
+    }
+
+    private void validate(String number) {
+        if (!isThreeNumbers(number)) {
+            throw new IllegalArgumentException("[ERROR] 숫자 야구공은 세 개의 숫자로 구성되어야 합니다.");
+        }
+        if (hasDuplicate(number)) {
+            throw new IllegalArgumentException("[ERROR] 숫자 야구공은 서로 중복될 수 없습니다.");
+        }
+    }
+
+    private boolean isThreeNumbers(String number) {
+        return number.matches("\\d{3}");
+    }
+
+    private boolean hasDuplicate(String number) {
+        return number.length() != number.chars()
+                .distinct()
+                .count();
+    }
+}
