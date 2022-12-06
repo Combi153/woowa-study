fec1c8dfa872a6fd169a935ceb6ab4c3043ab60f
chanminmaxscaler <jcm04@naver.com>
2022-12-06 22:24:55 +0900

    feat: InputView 클래스에 당첨번호 입력 기능을 구현한다
    readWinningNumbers 메소드는 사용자에게 콘솔로 당첨번호를 입력받는다.
    당첨번호는 validateBy 메소드로 검증한 후, List<Integer> 자료형으로 반환된다.

--- src/main/java/precourse/week3/view/InputView.java
+++ src/main/java/precourse/week3/view/InputView.java
@@ -2,25 +2,45 @@ package precourse.week3.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputView {
+
+    private static final String WINNING_NUMBERS_DELIMITER = ",";
 
     public int readPayMoney() {
         print(Message.PAY_GUIDE_MESSAGE);
-        return toInt(Console.readLine());
+        String input = Console.readLine();
+        validateBy(input, Validation.FOR_MONEY);
+        return toInt(input);
     }
 
-    private int toInt(String input) {
-        validate(input);
-        return Integer.parseInt(input);
-    }
-
-    private void validate(String input) {
-        if (!input.matches("\\d{1,}")) {
-            throw new IllegalArgumentException("[ERROR] 구입금액은 숫자로 입력해야 합니다.");
-        }
+    public List<Integer> readWinningNumbers() {
+        print(Message.WINNING_NUMBERS_GUIDE_MESSAGE);
+        String input = Console.readLine();
+        validateBy(input, Validation.FOR_WINNING_NUMBERS);
+        return toList(input);
     }
 
     private void print(Message message) {
         System.out.print(message.getMessage());
+    }
+
+    private void validateBy(String input, Validation validation) {
+        if (!input.matches(validation.getRegex())) {
+            throw new IllegalArgumentException(validation.getErrorMessage());
+        }
+    }
+
+    private int toInt(String input) {
+        return Integer.parseInt(input);
+    }
+
+    private List<Integer> toList(String input) {
+        return Arrays.stream(input.split(WINNING_NUMBERS_DELIMITER))
+                .map(this::toInt)
+                .collect(Collectors.toList());
     }
 }
---
+++ src/main/java/precourse/week3/view/Validation.java
@@ -0,0 +1,23 @@
+package precourse.week3.view;
+
+public enum Validation {
+    FOR_MONEY("\\d{1,}", "[ERROR] 구입금액은 숫자로 입력해야 합니다."),
+    FOR_WINNING_NUMBERS("\\d{1,},\\d{1,},\\d{1,},\\d{1,},\\d{1,},\\d{1,}", "[ERROR] 당첨 번호 입력 형식이 맞지 않습니다."),
+    FOR_BONUS_NUMBER("\\d{1,}", "[ERROR] 보너스 번호 입력 형식이 맞지 않습니다.");
+
+    private final String regex;
+    private final String errorMessage;
+
+    Validation(String regex, String errorMessage) {
+        this.regex = regex;
+        this.errorMessage = errorMessage;
+    }
+
+    public String getRegex() {
+        return regex;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+}
