edda72a21827fde522f30f8bced42ebbb162b713
chanminmaxscaler <jcm04@naver.com>
2022-12-07 00:37:39 +0900

    feat: Message 열겨형 객체에 상수를 추가한다
    WINNING_NUMBERS_GUIDE_MESSAGE,
    BONUS_NUMBER_GUIDE_MESSAGE 를 추가한다.

--- src/main/java/precourse/week3/view/Message.java
+++ src/main/java/precourse/week3/view/Message.java
@@ -2,7 +2,9 @@ package precourse.week3.view;
 
 public enum Message {
 
-    PAY_GUIDE_MESSAGE("구입금액을 입력해 주세요.");
+    PAY_GUIDE_MESSAGE("구입금액을 입력해 주세요.\n"),
+    WINNING_NUMBERS_GUIDE_MESSAGE("당첨 번호를 입력해 주세요.\n"),
+    BONUS_NUMBER_GUIDE_MESSAGE("보너스 번호를 입력해 주세요.\n");
 
     private final String message;
 
