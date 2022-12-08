442dc5f74b475aa902ed1e4ac9a4d235facf7981
chanminmaxscaler <jcm04@naver.com>
2022-12-08 20:51:42 +0900

    fix: 구입 금액에 제한을 둘 수 있도록 입력에 대한 검증 기능을 수정한다
    Validation 객체를 수정한다.
    검증에 사용하는 정규식을 수정하여 구입금액에 1자리 이상 9자리 이하의 숫자만 입력할 수 있도록 수정한다.
    큰 숫자를 입력하여 나타날 수 있는 오류를 방지하기 위함이다.

--- src/main/java/precourse/week3/view/Validation.java
+++ src/main/java/precourse/week3/view/Validation.java
@@ -1,9 +1,9 @@
 package precourse.week3.view;
 
 public enum Validation {
-    FOR_MONEY("\\d{1,}", "[ERROR] 구입금액은 숫자로 입력해야 합니다."),
-    FOR_WINNING_NUMBERS("\\d{1,},\\d{1,},\\d{1,},\\d{1,},\\d{1,},\\d{1,}", "[ERROR] 당첨 번호 입력 형식이 맞지 않습니다."),
-    FOR_BONUS_NUMBER("\\d{1,}", "[ERROR] 보너스 번호 입력 형식이 맞지 않습니다.");
+    FOR_MONEY("\\d{1,9}", "[ERROR] 구입금액은 1자리 이상 9자리 이하 숫자로 입력해야 합니다."),
+    FOR_WINNING_NUMBERS("\\d{1,2},\\d{1,2},\\d{1,2},\\d{1,2},\\d{1,2},\\d{1,2}", "[ERROR] 당첨 번호 입력 형식이 맞지 않습니다."),
+    FOR_BONUS_NUMBER("\\d{1,2}", "[ERROR] 보너스 번호 입력 형식이 맞지 않습니다.");
 
     private final String regex;
     private final String errorMessage;
