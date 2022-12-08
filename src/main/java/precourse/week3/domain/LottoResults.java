89ec37a9ac824bc1d38ecbb4070ace0c7e429e3a
chanminmaxscaler <jcm04@naver.com>
2022-12-08 17:37:03 +0900

    feat: LottoResults 클래스에 수익률을 계산하는 기능을 구현한다
    calculateRateOfReturn 메소드는 로또 가격을 입력받고, 총수익을 가격으로 나누어 수익률을 구해 반환한다.

--- src/main/java/precourse/week3/domain/LottoResults.java
+++ src/main/java/precourse/week3/domain/LottoResults.java
@@ -1,29 +1,55 @@
 package precourse.week3.domain;
 
-import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoResults {
 
+    private final int INITIAL_VALUE = 0;
     private final List<LottoResult> results;
 
     public LottoResults(List<LottoResult> results) {
         this.results = results;
     }
 
-    public List<Integer> countByRank() {
-        List<Integer> counts = new ArrayList<>();
-        for (Rank rank : Rank.values()) {
-            counts.add(countFrequency(rank));
+    public Map<Ranking, Integer> countByRank() {
+        Map<Ranking, Integer> counts = new HashMap<>();
+        for (Ranking ranking : Ranking.values()) {
+            counts.put(ranking, countFrequency(ranking));
         }
         return counts;
     }
 
-    private int countFrequency(Rank rank) {
-        if (rank.isMatchingWithBonus() == null) {
-            return Collections.frequency(results, rank.getTrueLottoResult()) + Collections.frequency(results, rank.getFalseLottoResult());
+    public int calculateRateOfReturn(int lottoPrice) {
+        validatePrice(lottoPrice);
+        int totalReturn = calculateReturn();
+        return totalReturn / lottoPrice;
+    }
+
+    private void validatePrice(int price) {
+        if (price == INITIAL_VALUE) {
+            throw new ArithmeticException("[ERROR] 0으로 나눌 수 없습니다.");
         }
-        return Collections.frequency(results, rank.getLottoResult());
+    }
+
+    private int calculateReturn() {
+        int totalReturn = INITIAL_VALUE;
+        for (Ranking ranking : Ranking.values()) {
+            totalReturn += calculateEachReturn(ranking);
+        }
+        return totalReturn;
+    }
+
+    private int calculateEachReturn(Ranking ranking) {
+        return countFrequency(ranking) * ranking.getPrize();
+    }
+
+    private int countFrequency(Ranking ranking) {
+        if (ranking.isMatchingWithBonus() == null) {
+            return Collections.frequency(results, ranking.getTrueLottoResult()) + Collections.frequency(results, ranking.getFalseLottoResult());
+        }
+        return Collections.frequency(results, ranking.getLottoResult());
     }
 }
--- src/main/java/precourse/week3/domain/Ranking.java
+++ src/main/java/precourse/week3/domain/Ranking.java
@@ -1,19 +1,27 @@
 package precourse.week3.domain;
 
-public enum Rank {
+public enum Ranking {
 
-    FIRST_PLACE(6, false),
-    SECOND_PLACE(5, true),
-    THIRD_PLACE(5, false),
-    FOURTH_PLACE(4, null),
-    FIFTH_PLACE(3, null);
+    FIRST_PLACE(1, 6, false, 2_000_000_000),
+    SECOND_PLACE(2, 5, true, 30_000_000),
+    THIRD_PLACE(3, 5, false, 1_500_000),
+    FOURTH_PLACE(4, 4, null, 50_000),
+    FIFTH_PLACE(5, 3, null, 5_000);
 
+    private final int ranking;
     private final int numberOfMatching;
     private final Boolean isMatchingWithBonus;
+    private final int prize;
 
-    Rank(int numberOfMatching, Boolean isMatchingWithBonus) {
+    Ranking(int ranking, int numberOfMatching, Boolean isMatchingWithBonus, int prize) {
+        this.ranking = ranking;
         this.numberOfMatching = numberOfMatching;
         this.isMatchingWithBonus = isMatchingWithBonus;
+        this.prize = prize;
+    }
+
+    public int getRanking() {
+        return ranking;
     }
 
     public int getNumberOfMatching() {
@@ -22,6 +30,10 @@ public enum Rank {
 
     public Boolean isMatchingWithBonus() {
         return isMatchingWithBonus;
+    }
+
+    public int getPrize() {
+        return prize;
     }
 
     public LottoResult getLottoResult() {
