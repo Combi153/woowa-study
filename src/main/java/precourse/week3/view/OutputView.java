09b688f3cc81eb55cf53a1bb8ce444aa6070d908
chanminmaxscaler <jcm04@naver.com>
2022-12-09 22:26:16 +0900

    feat: 출력 기능을 OutputView 클래스에 구현한다

---
+++ src/main/java/precourse/week3/view/OutputView.java
@@ -0,0 +1,59 @@
+package precourse.week3.view;
+
+import precourse.week3.domain.Ranking;
+import precourse.week3.domain.lottoticket.LottoTickets;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class OutputView {
+
+    public void printLottoTickets(LottoTickets lottoTickets) {
+        print(String.format(Message.PAYMENT_MESSAGE.getMessage(), lottoTickets.size()));
+        print(lottoTickets.toMessage(Message.LOTTO_DELIMITER.getMessage(), Message.LOTTO_PREFIX.getMessage(), Message.LOTTO_SUFFIX.getMessage()));
+    }
+
+    public void printStatistics(Map<Ranking, Integer> rankingAndCount, int rateOfReturn) {
+        print(Message.STATISTICS_TITLE_MESSAGE.getMessage());
+        printRankingAndCount(rankingAndCount);
+        print(String.format(Message.RATE_OF_RETURN_MESSAGE.getMessage(), rateOfReturn));
+    }
+
+    private void printRankingAndCount(Map<Ranking, Integer> rankingAndCount) {
+        List<Map.Entry<Ranking, Integer>> entries = makeAscendingEntries(rankingAndCount);
+        for (Map.Entry<Ranking, Integer> entry : entries) {
+            Ranking ranking = entry.getKey();
+            int count = entry.getValue();
+            String message = makeStatisticsMessageBy(ranking, count);
+            print(message);
+        }
+    }
+
+    private List<Map.Entry<Ranking, Integer>> makeAscendingEntries(Map<Ranking, Integer> rankingAndCount) {
+        List<Map.Entry<Ranking, Integer>> entries = rankingAndCount.entrySet().stream()
+                .sorted(Comparator.comparing(entry -> entry.getKey().getRanking()))
+                .collect(Collectors.toList());
+        Collections.reverse(entries);
+        return entries;
+    }
+
+    private String makeStatisticsMessageBy(Ranking ranking, int count) {
+        int prize = ranking.getPrize();
+        int numberOfMatching = ranking.getNumberOfMatching();
+        return String.format(getStatisticsMessageBy(ranking), numberOfMatching, prize, count);
+    }
+
+    private static String getStatisticsMessageBy(Ranking ranking) {
+        if (ranking.getRanking() == 2) {
+            return Message.STATISTICS_BONUS_CONTENT_MESSAGE.getMessage();
+        }
+        return Message.STATISTICS_NORMAL_CONTENT_MESSAGE.getMessage();
+    }
+
+    public void print(String message) {
+        System.out.print(message);
+    }
+}
