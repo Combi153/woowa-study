1a4500c3315f1466346314c8ada7b84d1bdc0e98
chanminmaxscaler <jcm04@naver.com>
2022-12-09 22:25:13 +0900

    feat: Application의 기능을 구현한다

--- src/main/java/precourse/week3/Application.java
+++ src/main/java/precourse/week3/Application.java
@@ -1,7 +1,12 @@
 package precourse.week3;
 
+import precourse.week3.controller.LottoController;
+import precourse.week3.view.InputView;
+import precourse.week3.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoController lottoController = new LottoController(new InputView(), new OutputView());
+        lottoController.runLottoProgramMain();
     }
 }
