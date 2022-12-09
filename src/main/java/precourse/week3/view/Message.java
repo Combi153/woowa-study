d6cecc1d2e81809cb90ce9695baebe6c1f011129
chanminmaxscaler <jcm04@naver.com>
2022-12-09 22:26:56 +0900

    feat: 출력 메시지에 대한 Enum 클래스를 구현한다

--- src/main/java/precourse/week3/view/Message.java
+++ src/main/java/precourse/week3/view/Message.java
@@ -3,8 +3,16 @@ package precourse.week3.view;
 public enum Message {
 
     PAY_GUIDE_MESSAGE("구입금액을 입력해 주세요.\n"),
+    PAYMENT_MESSAGE("%d개를 구매했습니다.\n"),
+    LOTTO_DELIMITER(", "),
+    LOTTO_PREFIX("["),
+    LOTTO_SUFFIX("]\n"),
     WINNING_NUMBERS_GUIDE_MESSAGE("당첨 번호를 입력해 주세요.\n"),
-    BONUS_NUMBER_GUIDE_MESSAGE("보너스 번호를 입력해 주세요.\n");
+    BONUS_NUMBER_GUIDE_MESSAGE("보너스 번호를 입력해 주세요.\n"),
+    STATISTICS_TITLE_MESSAGE("당첨 통계\n---\n"),
+    STATISTICS_NORMAL_CONTENT_MESSAGE("%d개 일치 (%s원) - %d개\n"),
+    STATISTICS_BONUS_CONTENT_MESSAGE("%d개 일치, 보너스 볼 일치 (%s원) - %d개\n"),
+    RATE_OF_RETURN_MESSAGE("총 수익률은 %s입니다.\n");
 
     private final String message;
 
