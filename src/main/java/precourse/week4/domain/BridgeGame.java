142b86bbab3ea1b6f95e60dfb7bf8d98a4bcc361
chanminmaxscaler <jcm04@naver.com>
2022-12-12 21:44:25 +0900

    chore: 클래스의 패키지 구조를 변경한다

--- src/main/java/precourse/week4/domain/BridgeGame.java
+++ src/main/java/precourse/week4/domain/BridgeGame.java
@@ -1,4 +1,4 @@
-package precourse.week4;
+package precourse.week4.domain;
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
--- src/main/java/precourse/week4/view/InputView.java
+++ src/main/java/precourse/week4/view/InputView.java
@@ -1,4 +1,4 @@
-package precourse.week4;
+package precourse.week4.view;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
