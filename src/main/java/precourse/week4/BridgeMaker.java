f06dc5b5dfa9d1cb4199bd27abdcf420d4b93803
chanminmaxscaler <jcm04@naver.com>
2022-12-12 23:04:00 +0900

    feat: BridgeMaker 클래스에 다리 방향 생성 기능을 구현한다
    makeBridge 메소드는 다리 길이를 입력받아, 입력 받은 길이에 해당하는 다리의 방향 목록을 반환한다.
    다리를 만들기 전에 다리의 길이를 검증한다.
    다리의 길이가 3이상 20이하의 범위를 벗어나면 오류를 발생시킨다.

--- src/main/java/precourse/week4/BridgeMaker.java
+++ src/main/java/precourse/week4/BridgeMaker.java
@@ -1,5 +1,6 @@
 package precourse.week4;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,6 +8,11 @@ import java.util.List;
  */
 public class BridgeMaker {
 
+    private static final int START_OF_BRIDGE_RANGE = 3;
+    private static final int END_OF_BRIDGE_RANGE = 20;
+    private static final int UPWARD_NUMBER = 1;
+    public static final String UPWARD_DIRECTION = "U";
+    public static final String DOWNWARD_DIRECTION = "D";
     private final BridgeNumberGenerator bridgeNumberGenerator;
 
     public BridgeMaker(BridgeNumberGenerator bridgeNumberGenerator) {
@@ -18,6 +24,33 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        validate(size);
+        return combineBridgeFactor(size);
+    }
+
+    private List<String> combineBridgeFactor(int size) {
+        List<String> bridge = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            bridge.add(makeBridgeFactor());
+        }
+        return bridge;
+    }
+
+    private String makeBridgeFactor() {
+        int number = bridgeNumberGenerator.generate();
+        if (number == UPWARD_NUMBER) {
+            return UPWARD_DIRECTION;
+        }
+        return DOWNWARD_DIRECTION;
+    }
+
+    private void validate(int size) {
+        if (isOutOfRange(size)) {
+            throw new IllegalArgumentException("[ERROR] 입력된 다리 길이가 범위를 벗어났습니다.");
+        }
+    }
+
+    private boolean isOutOfRange(int size) {
+        return size < START_OF_BRIDGE_RANGE || size > END_OF_BRIDGE_RANGE;
     }
 }
