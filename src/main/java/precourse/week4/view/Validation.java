b874889b9bdbcc2c07184d3294377f1208df181e
chanminmaxscaler <jcm04@naver.com>
2022-12-13 19:58:04 +0900

    feat: 명령(재시도, 종료) 입력 기능을 구현한다
    InputView 객체의 readGameCommand 메소드는 사용자에게 명령을 입력받는다.
    사용자의 명령이 문자 하나가 아니라면 오류를 발생시킨다.

--- src/main/java/precourse/week4/view/Validation.java
+++ src/main/java/precourse/week4/view/Validation.java
@@ -3,7 +3,8 @@ package precourse.week4.view;
 public enum Validation {
 
     BRIDGE_SIZE_FORM("\\d{1,2}", "[ERROR] 다리 길이 입력 형식에 맞지 않습니다."),
-    MOVING_COMMAND_FORM("\\D{1}", "[ERROR] 방향 형식에 맞지 않습니다.");
+    MOVING_COMMAND_FORM("\\D{1}", "[ERROR] 방향은 문자 하나로 입력해야 합니다."),
+    PLAYING_COMMAND_FORM("\\D{1}", "[ERROR] 명령어는 문자 하나로 입력해야 합니다.");
 
     private final String form;
     private final String errorMessage;
